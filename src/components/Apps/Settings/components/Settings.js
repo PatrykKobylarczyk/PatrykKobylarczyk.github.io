8026b42292895b420bc1e5c77fcb3b29f8aa6232
PatrykKobylarczyk <patryk.kobylarczyk@gmail.com>
2022-04-24 21:43:26 +0200

    erge with a master branch

---
+++ src/components/Apps/Settings/components/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/Settings.scssnpm '
+
+const Settings = () => {
+    return ( 
+        <div>
+        
+        </div>
+     );
+}
+ 
+export default Settings;
